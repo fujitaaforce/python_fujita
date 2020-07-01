f9417b85577ab148eee47bb7811620352a184116
Kosuke <k.fujita@a-force.co.jp>
2020-07-01 17:04:58 +0900

    refs #1: python1の課題提出二回目

--- practice1-2.py
+++ practice1-2.py
@@ -3,7 +3,5 @@ print("一つ目の値を入力してください")
 num1 = int(input())
 print("二つ目の値を入力してください")
 num2 = int(input())
-num3 = num1+num2
-num4 = (num1+num2)/2
-print(num3)
-print(num4)
+print("合計：",num1+num2)
+print("平均：",(num1+num2)/2)
