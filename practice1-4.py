4aeaa2c419ad89403345743b2decd3084eabc6c7
Kosuke <k.fujita@a-force.co.jp>
2020-07-02 17:37:24 +0900

    refs #1: python1の課題提出三回目

--- practice1-4.py
+++ practice1-4.py
@@ -3,21 +3,15 @@ from decimal import Decimal, ROUND_HALF_UP
 debt = 250000
 repay = 30000
 i = 1
+
 while debt > 0:
-    if debt < 30000:
-        interest = Decimal(str(debt * (0.14 / 12))).quantize(Decimal('0.0000000001'), ROUND_HALF_UP)
-        debt += interest
-        if debt > 30000:
-            debt -= repay
-            msg = "残り" + str(debt) + "円"
-        else: 
-            repay = debt
-            debt -= repay
-            msg = "返済完了"
+    interest = debt * (0.14 / 12)
+    debt += interest
+    if debt <= 29654:
+        repay = debt
+        msg = "返済完了"
     else:
-        interest = Decimal(str(debt * (0.14 / 12))).quantize(Decimal('0.0000000001'), ROUND_HALF_UP)
-        debt += interest
-        debt -= repay
         msg = "残り" + str(debt) + "円"
+    debt -= repay
     print(str(i) + "か月目：返済額=" + str(repay) + "円,"+ msg)
     i += 1
