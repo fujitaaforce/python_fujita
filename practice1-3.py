e3e445a6de8285871c9c0602423f9160a64a3e31
Kosuke <k.fujita@a-force.co.jp>
2020-07-02 16:52:55 +0900

    refs #1: python1の課題提出三回目

--- practice1-3.py
+++ practice1-3.py
@@ -1,14 +1,14 @@
-#課題3
+# 課題3
 weight = 50
 hight = 1.7
-bmi = weight/(hight**2)
-msg=""
+bmi = weight / (hight ** 2)
+msg = ""
 if bmi<18.5:
-    msg="やせ"
-if 18.5<=bmi<25:
-    msg="標準"
-if 25<=bmi<30:
-    msg="肥満"
-if 30<=bmi:
-    msg="高度肥満"
-print("あなたは"+msg+"です。")
+    msg = "やせ"
+if 18.5 <= bmi < 25:
+    msg = "標準"
+if 25 <= bmi < 30:
+    msg = "肥満"
+if 30 <= bmi:
+    msg = "高度肥満"
+print("あなたは「" + msg + "」です。")
--- practice1-4.py
+++ practice1-4.py
@@ -1,25 +1,23 @@
-#課題4
-debt=250000
-repay=30000
-i=1
-
-while debt>0:
-    if debt<30000:
-        interest=(debt*0.14)/12
-        if (debt+interest)>30000:
-            debt+=interest
-            debt-=repay
-            msg="残り",debt,"円"
-        else:
-            debt+=interest
-            repay=debt
-            debt-=repay
-            msg="返済完了"
+# 課題4
+from decimal import Decimal, ROUND_HALF_UP
+debt = 250000
+repay = 30000
+i = 1
+while debt > 0:
+    if debt < 30000:
+        interest = Decimal(str(debt * (0.14 / 12))).quantize(Decimal('0.0000000001'), ROUND_HALF_UP)
+        debt += interest
+        if debt > 30000:
+            debt -= repay
+            msg = "残り" + str(debt) + "円"
+        else: 
+            repay = debt
+            debt -= repay
+            msg = "返済完了"
     else:
-        interest=(debt*0.14)/12
-        debt+=interest
-        debt-=repay
-        msg="残り",debt,"円"
-    print(i,"か月目返済額=",repay,"円,",msg)
-    i+=1
-
+        interest = Decimal(str(debt * (0.14 / 12))).quantize(Decimal('0.0000000001'), ROUND_HALF_UP)
+        debt += interest
+        debt -= repay
+        msg = "残り" + str(debt) + "円"
+    print(str(i) + "か月目：返済額=" + str(repay) + "円,"+ msg)
+    i += 1
