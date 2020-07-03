075a076ab8520f9ce1b52515dcbb29545948ae09
Kosuke <k.fujita@a-force.co.jp>
2020-07-03 14:30:40 +0900

    refs #3: python2の課題提出三回目

--- python2_test2.py
+++ python2_test2.py
@@ -2,6 +2,10 @@ import datetime
 
 def calcurate(year):
     if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                msg = "閏年です。"
+            msg = "閏年ではありません。"
         msg = "閏年です。"
     else:
         msg = "閏年ではありません。"
@@ -9,6 +13,10 @@ def calcurate(year):
 now = datetime.date.today()
 print("現在日は西暦{}年{}月{}日です。".format(now.year, now.month, now.day))
 year = now.year
-print("現在日は" + calcurate(year))
-print("昨年の同じ日は" + calcurate(year - 1))
-print("来年の同じ日は" + calcurate(year + 1))
+i = -1
+while i<2 :
+    msg = calcurate(year+i)
+    i += 1
+print("現在日は" + msg)
+print("昨年の同じ日は" + msg)
+print("来年の同じ日は" + msg)
