c78bacb941e2ace6ba3d97cac98381d1b980715d
Kosuke <k.fujita@a-force.co.jp>
2020-07-07 10:37:50 +0900

    refs #5: python3の課題提出二回目

--- python3_test1.py
+++ python3_test1.py
@@ -3,10 +3,13 @@
 class MP3Player :
     def play_music(self) :
         print("音楽を再生します。")
+
     def next_song(self) :
         print("次の曲を再生します。")
+
     def previous_song(self) :
         print("前の曲を再生します。")
+
     def stop_music(self) :
         print("音楽を止めます。")
 
@@ -20,8 +23,10 @@ class CellPhone :
     def __init__(self, tel_number, mail_address) :
         self.tel_number = tel_number
         self.mail_address = mail_address
+
     def call(self):
         print("{}から発信します。".format(self.tel_number))
+        
     def send_mail(self):
         print("{}から送信します。".format(self.mail_address))
 
